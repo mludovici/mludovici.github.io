32e8e15455aec2cfd834c052ce31f71539ca01dd
Marc <ludovicimarc@gmail.com>
2021-09-29 15:56:02 +0200

    fix buttno profilepage

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -63,7 +63,7 @@ function ProfilePage() {
                             </li>
                         </ul>
                         <button
-                            className={`${ProfilePageCSS['button']} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded float-right`}>
+                            className={`${ProfilePageCSS['btn']} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded float-right`}>
                             {!editMode ? 'Edit' : 'Update'}
                         </button>
                     </div>
