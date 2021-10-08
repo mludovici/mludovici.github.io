864e7b8cb10a8fdfc69dc27187a2dc5411c70bf4
Marc <ludovicimarc@gmail.com>
2021-10-08 11:43:56 +0200

    bugfixes

--- src/components/Games/QuizList.jsx
+++ src/components/Games/QuizList.jsx
@@ -14,6 +14,9 @@ function QuizList({ quizData, resetGame }) {
         setHasChosen(false)
         setChecked(null)
         if (gameFinished) {
+            analytics.logEvent('game_finished', {
+                score: `${correctAnswerCount} of ${quizData.length}`,
+            })
             return
         }
 
@@ -67,47 +70,45 @@ function QuizList({ quizData, resetGame }) {
                     nrOfQuestions={quizData.length}
                     questionNr={countNr + 1}></Quizcard>
             )}
-            {gameFinished &&
-                analytics.logEvent('game_finished', {
-                    score: correctAnswerCount / quizData.length,
-                }) && (
-                    <div className="max-w-md mx-auto bg-gray-200 rounded-md shadow-quizBox border-1 border-gray-300 mt-10">
-                        <img
-                            src={trophySVG}
-                            className={`mx-auto h-40 w-40 pt-3`}
-                            alt="congrats"
-                        />
-                        <div
-                            id="quizFinishedSection"
-                            className="mt-5 flex justify-around bg-blue-600">
-                            <p className="">
-                                {' '}
-                                You finished with a score of{' '}
-                                {correctAnswerCount} / {quizData.length}
-                            </p>
+            {gameFinished && (
+                <div className="max-w-md mx-auto bg-gray-200 rounded-md shadow-quizBox border-1 border-gray-300 mt-10">
+                    <img
+                        src={trophySVG}
+                        className={`mx-auto h-40 w-40 pt-3`}
+                        alt="congrats"
+                    />
+                    <div
+                        id="quizFinishedSection"
+                        className="mt-5 flex justify-around bg-blue-600">
+                        <p className="">
+                            {' '}
+                            You finished with a score of {
+                                correctAnswerCount
+                            } / {quizData.length}
+                        </p>
 
-                            <button
-                                id="resetGame"
-                                onClick={resetGame}
-                                className="bg-blue-300 hover:bg-blue-400 text-white font-bold py-0 px-0 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-10">
-                                Replay
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className={`h-5 w-5 inline
+                        <button
+                            id="resetGame"
+                            onClick={resetGame}
+                            className="bg-blue-300 hover:bg-blue-400 text-white font-bold py-0 px-0 border-b-4 border-blue-700 hover:border-blue-500 rounded ml-10">
+                            Replay
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={`h-5 w-5 inline
                                 }
                                 }`}
-                                    viewBox="0 0 20 20"
-                                    fill="black">
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                                viewBox="0 0 20 20"
+                                fill="black">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </button>
                     </div>
-                )}
+                </div>
+            )}
         </div>
     )
 }
