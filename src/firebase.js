090e80bceb2bdb85e7eaf2ced1a36cf39862123c
Marc <ludovicimarc@gmail.com>
2021-10-11 14:32:09 +0200

    mod of env usage for heroku ...

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import env from './env.js'
+//import env from './env.js'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
@@ -6,14 +6,14 @@ import 'firebase/storage'
 import 'firebase/analytics'
 
 const firebaseConfig = {
-    apiKey: env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: env.REACT_APP_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 export const app = firebase.initializeApp(firebaseConfig)
